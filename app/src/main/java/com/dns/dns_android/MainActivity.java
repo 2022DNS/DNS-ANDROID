284a9761ebaae3e42ef377f79c80c85ffb7883a3
Sohn Young Jin <sonkim1001@naver.com>
2022-09-12 15:42:13 +0900

    Add audio permission request and multiple permission request function

--- app/src/main/java/com/dns/dns_android/MainActivity.java
+++ app/src/main/java/com/dns/dns_android/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dns.dns_android;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -12,13 +13,19 @@ import com.dns.dns_lib.DnsPermission;
 import com.dns.dns_lib.DnsRecognition;
 import com.dns.dns_lib.DnsRecognitionListener;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+    private final int PERMISSION_REQUEST_CODE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (DnsPermission.requestCameraPermission(MainActivity.this, this, 123)) {
+        String[] permissions = new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA};
+        if (DnsPermission.requestPermissions(MainActivity.this, this, PERMISSION_REQUEST_CODE, new ArrayList<>(Arrays.asList(permissions)))) {
             showCameraPreview();
         }
     }
@@ -27,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
-            case 123:
+            case PERMISSION_REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(MainActivity.this, "Permission granted", Toast.LENGTH_SHORT).show();
                     showCameraPreview();
--- dns_lib/src/main/java/com/dns/dns_lib/DnsPermission.java
+++ dns_lib/src/main/java/com/dns/dns_lib/DnsPermission.java
@@ -12,6 +12,9 @@ import android.provider.Settings;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * DnsPermission class is for easily obtaining camera permission. If the permission is denied, it moves to the application permission setting screen through its own dialog.
  *
@@ -23,8 +26,8 @@ public class DnsPermission {
     /**
      * Request camera permission.
      *
-     * @param context Context.
-     * @param activity Activity.
+     * @param context     Context.
+     * @param activity    Activity.
      * @param requestCode Request code for onRequestPermissionResult().
      * @return Return true when camera permission already granted.
      */
@@ -50,6 +53,67 @@ public class DnsPermission {
             // Camera permission is not granted.
             ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, requestCode);
         }
+
+        return false;
+    }
+
+    /**
+     * Request audio permission.
+     *
+     * @param context     Context.
+     * @param activity    Activity.
+     * @param requestCode Request code for onRequestPermissionResult().
+     * @return Return true when audio permission already granted.
+     */
+    public static boolean requestAudioPermission(Context context, Activity activity, int requestCode) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            // Audio permission already granted.
+            return true;
+        }
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.RECORD_AUDIO)) {
+            // Audio permission denied and need to show request permission rationale.
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+            alertDialogBuilder.setCancelable(true);
+            alertDialogBuilder.setTitle("Audio Permission");
+            alertDialogBuilder.setMessage("You need audio permission to use this service.");
+            alertDialogBuilder.setPositiveButton("Setting", (dialogInterface, i) -> {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", context.getPackageName(), null));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                activity.startActivity(intent);
+            });
+            alertDialogBuilder.show();
+        } else {
+            // Audio permission is not granted.
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.RECORD_AUDIO}, requestCode);
+        }
+
+        return false;
+    }
+
+    /**
+     * Request permissions.
+     *
+     * @param context     Context.
+     * @param activity    Activity.
+     * @param requestCode Request code for onRequestPermissionResult().
+     * @param permissions Request permissions.
+     * @return Return true when permissions already granted.
+     */
+    public static boolean requestPermissions(Context context, Activity activity, int requestCode, ArrayList<String> permissions) {
+        List<String> notGrantedPermissions = new ArrayList<>();
+
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                notGrantedPermissions.add(permission);
+            }
+        }
+
+        if (notGrantedPermissions.size() == 0) {
+            return true;
+        }
+
+        ActivityCompat.requestPermissions(activity, notGrantedPermissions.toArray(new String[notGrantedPermissions.size()]), requestCode);
 
         return false;
     }
